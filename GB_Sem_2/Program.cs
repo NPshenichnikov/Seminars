b7b10c03c57c543163338f02bc43ffd65ed8308f
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-16 21:56:02 +0300

    Задача 9

---
+++ GB_Sem_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 9.Напишите программу,которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
+
+void ShowNumber(){
+    int num = new Random().Next(10,100);
+    int firstNum = num / 10;
+    int secondNum = num %10;
+    Console.Write(num + " => ");
+    if (firstNum > secondNum){
+        Console.WriteLine(firstNum);
+    }
+    else{
+        Console.WriteLine(secondNum);
+    }
+}
+ShowNumber();
