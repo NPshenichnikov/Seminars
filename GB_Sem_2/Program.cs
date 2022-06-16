ea4b7daaae14380cc3b9f6317ce5fab5afb7c040
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-17 01:05:57 +0300

    Задача 14

--- GB_Sem_2/Program.cs
+++ GB_Sem_2/Program.cs
@@ -30,18 +30,35 @@
 //  12.Напишите программу,которая будет принимать на вход два числа и выводить,является ли второе число кратным первому.Если число 2 не кратно числу 1,то программа выводит остаток от деления.
 
 
-void ResultNum(int num1, int num2){
-    if(num2 % num1 == 0){
-    Console.WriteLine(num2 + " кратно " + num1);
-    }
+// void ResultNum(int num1, int num2){
+//     if(num2 % num1 == 0){
+//     Console.WriteLine(num2 + " кратно " + num1);
+//     }
+//     else{
+//     Console.WriteLine(num2 + " не кратно " + num1 + ", остаток - " + num2 % num1);
+//     }
+// }
+
+// Console.Write("input first number : ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input second number : ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// ResultNum(num1, num2);
+
+
+//  14.Напишите программу,которая принимает на вход число и проверяет,кратно ли оно одновременно 7 и 23.
+
+
+void Result(int num){
+    if(num % 7 == 0 && num % 23 == 0)
+        Console.WriteLine(num + " кратно 7 и 23");
     else{
-    Console.WriteLine(num2 + " не кратно " + num1 + ", остаток - " + num2 % num1);
+        Console.WriteLine(num + " не кратно 7 и 23");
     }
 }
 
-Console.Write("input first number : ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("input second number : ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-ResultNum(num1, num2);
+Result(num);
