1ed723a50f797d13b29c155726c233d426ed241d
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-17 00:38:29 +0300

    Задача 12

--- GB_Sem_2/Program.cs
+++ GB_Sem_2/Program.cs
@@ -17,11 +17,31 @@
 
 // 11.Напишите программу,которая выводит случайное трёхзначное число и удаляетвторую цифру этого числа.
 
-int NumberWithoutSecDig(int Number){
-    int NewNumber = 0;
-    NewNumber = Number / 100 * 10 + Number % 10;
-    return NewNumber;
+// int NumberWithoutSecDig(int Number){
+//     int NewNumber = 0;
+//     NewNumber = Number / 100 * 10 + Number % 10;
+//     return NewNumber;
+// }
+// int Number = new Random().Next(100,1000);
+// int a = NumberWithoutSecDig(Number);
+// Console.WriteLine(Number + " " + a);
+
+
+//  12.Напишите программу,которая будет принимать на вход два числа и выводить,является ли второе число кратным первому.Если число 2 не кратно числу 1,то программа выводит остаток от деления.
+
+
+void ResultNum(int num1, int num2){
+    if(num2 % num1 == 0){
+    Console.WriteLine(num2 + " кратно " + num1);
+    }
+    else{
+    Console.WriteLine(num2 + " не кратно " + num1 + ", остаток - " + num2 % num1);
+    }
 }
-int Number = new Random().Next(100,1000);
-int a = NumberWithoutSecDig(Number);
-Console.WriteLine(Number + " " + a);
+
+Console.Write("input first number : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second number : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+ResultNum(num1, num2);
