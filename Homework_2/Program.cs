0f302a0471aee30e152c474d4475e7c0d231c323
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-17 22:16:33 +0300

    Выполнена задача 2

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,19 +1,53 @@
 ﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+// void Numresult(int num){
+//     if(num > 100 && num < 1000){
+//     num = num / 10 % 10;
+//        Console.WriteLine(" Вторая цифра этого числа: " + num);
+//     }
+//     else{
+//        Console.WriteLine(" Введите корректное число!!!");
+//     }
+// }
+
+// Console.Write(" Введите трёхзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Numresult(num);
+
+
+// 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
 void Numresult(int num){
-    if(num > 100 && num < 1000){
-    num = num / 10 % 10;
-       Console.WriteLine(" Вторая цифра этого числа: " + num);
+    while(num >= 1000){
+    num = num / 10;
     }
-    else{
-       Console.WriteLine(" Введите корректное число!!!");
-    }
+    if(num < 100) Console.WriteLine(" Третья цифра отсутствует ");
+    if(num >=100 && num < 1000) Console.WriteLine(" Третья цифра: " + (num = num % 10));
 }
 
-Console.Write(" Введите трёхзначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());      
 
 Numresult(num);
+
+      
+      
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
 
 
 
