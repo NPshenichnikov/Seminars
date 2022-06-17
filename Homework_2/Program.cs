c91c540aa3feceb4dbeee2e6a41caa004585f2a4
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-17 20:06:21 +0300

    Исправлена 1 задача

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,15 +1,19 @@
 ﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.Write(" Введите трёхзначное число ");
-int num = Convert.ToInt32(Console.ReadLine());
-if(num > 100 && num < 1000){
+void Numresult(int num){
+    if(num > 100 && num < 1000){
     num = num / 10 % 10;
-    Console.WriteLine(" Вторая цифра этого числа: " + num);
-}
-else{
-    Console.WriteLine(" Введено некорректное число");
+       Console.WriteLine(" Вторая цифра этого числа: " + num);
+    }
+    else{
+       Console.WriteLine(" Введите корректное число!!!");
+    }
 }
 
+Console.Write(" Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Numresult(num);
 
 
 
