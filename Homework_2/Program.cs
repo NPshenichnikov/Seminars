659e65ffda283d4b8092d636f1f974136d63f910
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-18 00:15:13 +0300

    Решение задачи 3

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -18,35 +18,32 @@
 
 // 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-void Numresult(int num){
-    while(num >= 1000){
-    num = num / 10;
-    }
-    if(num < 100) Console.WriteLine(" Третья цифра отсутствует ");
-    if(num >=100 && num < 1000) Console.WriteLine(" Третья цифра: " + (num = num % 10));
-}
+// void Numresult(int num){
+//     while(num >= 1000){
+//     num = num / 10;
+//     }
+//     if(num < 100) Console.WriteLine(" Третья цифра отсутствует ");
+//     if(num >=100 && num < 1000) Console.WriteLine(" Третья цифра: " + (num = num % 10));
+// }
 
-Console.Write(" Введите целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());      
+// Console.Write(" Введите целое число: ");
+// int num = Convert.ToInt32(Console.ReadLine());      
 
-Numresult(num);
+// Numresult(num);
 
       
-      
+ //   3. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.   
 
+void Weekend(int day){
+if(day == 6 || day == 7) Console.WriteLine(" Ура!!!Выходной!!! ");
+if(day > 0 && day < 6) Console.WriteLine(" Рабочий день( ");
+if(day < 1 || day > 7) Console.WriteLine(" Некорректное число ");
+}   
 
+Console.Write("Введите число дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-
-
-    
-    
-
-
-
-
+Weekend(day);
 
 
 
