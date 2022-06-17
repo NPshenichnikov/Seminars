842180833a9b8c785691fc181bcbfa43e6c40a02
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-17 19:53:37 +0300

    Решена задача 1

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write(" Введите трёхзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 100 && num < 1000){
+    num = num / 10 % 10;
+    Console.WriteLine(" Вторая цифра этого числа: " + num);
+}
+else{
+    Console.WriteLine(" Введено некорректное число");
+}
+
+
+
+
+
+
