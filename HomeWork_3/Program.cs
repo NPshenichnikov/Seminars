07a9d95d613883b8b382ee65b400460622316779
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-20 23:21:17 +0300

    Решил задачу 19

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Задача 19:Напишите программу,которая принимает на вход пятизначное число и проверяет,является ли оно палидромом.
+
+// void Polydrome(int num){
+
+//     if(num < 10000 || num >99999) Console.WriteLine("Некорректное число");
+
+//     else{
+//         if((num / 10000 == num % 10) && (num / 1000 % 10 == num % 100 / 10))
+//             Console.WriteLine("Число является палиндромом");
+//         else{
+//             Console.WriteLine("Число не является палиндромом");
+//         }
+//     }
+// }
+
+// Console.Write(" Введите пятизначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Polydrome(num);
+
+
