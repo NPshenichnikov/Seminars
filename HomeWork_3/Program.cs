a4f8dab395aad56d63670e8604bfb232c28ff561
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-21 22:50:40 +0300

    Решил задачу 21

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -19,3 +19,28 @@
 // Polydrome(num);
 
 
+//  Задача 21:Напишите программу,которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.Clear();
+// double LengthSeg(int x1, int y1, int z1, int x2, int y2, int z2){
+//     return Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+// }
+
+// Console.Write("Введите координату X1: ");
+// int xA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Y1: ");
+// int yA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Z1: ");
+// int zA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату X2: ");
+// int xB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Y2: ");
+// int yB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату Z2: ");
+// int zB = Convert.ToInt32(Console.ReadLine());
+
+// double res = Math.Round(LengthSeg(x1:xA, y1:yA, x2:xB, y2:yB, z1:zA, z2:zB),2);
+// Console.WriteLine("Длина отрезка: " + res);
+
+
+
