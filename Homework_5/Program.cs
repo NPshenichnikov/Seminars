479299eb304baefd6075eb6ae844b07f39f00964
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-27 01:06:03 +0300

    Решил задачу 2

---
+++ Homework_5/Program.cs
@@ -0,0 +1,86 @@
+﻿//   1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+// int[] CreateRandomArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// int[] MyArray = CreateRandomArray(6, 100, 1000);
+
+// void ShowArray(int[] array)
+// {
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+
+// }
+
+// ShowArray(MyArray);
+
+// int sum = 0;
+
+// void ArrayCheck(int[] array)
+// {
+//     for (int i = 0; i < MyArray.Length; i++)
+//     {
+//         if (MyArray[i] % 2 == 0) sum++;
+         
+//     }
+// }
+
+// ArrayCheck(MyArray);
+// Console.WriteLine("-> " + sum);
+
+
+//  2. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// int[] CreateRandomArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// int[] MyArray = CreateRandomArray(6, -20, 20);
+
+// void ShowArray(int[] array)
+// {
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+
+// }
+
+// ShowArray(MyArray); 
+
+// int sum = 0;
+
+// void ArrayCheck(int[] array)
+// {
+//     for (int i = 1; i < MyArray.Length; i = i + 2)
+//     {
+//         sum = sum + MyArray[i];
+//     }
+// }
+
+// ArrayCheck(MyArray);
+// Console.WriteLine("-> " + sum);
+
+
+
+
+
+
