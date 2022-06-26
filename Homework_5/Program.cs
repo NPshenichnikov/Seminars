f6bc8379bfcbc6d8aa57654793d60b445b2d537c
«Nikolay <pshenichnikovnf@mail.ru>
2022-06-27 01:35:39 +0300

    Решил задачу 3

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -80,7 +80,43 @@
 // Console.WriteLine("-> " + sum);
 
 
+//  3. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
+// int[] CreateRandomArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
 
+// int[] MyArray = CreateRandomArray(6, 1, 50);
 
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
+
+// ShowArray(MyArray); 
+
+// int min = 50;
+// int max = 1; 
+
+// void ArrayCheck(int[] array)
+// {
+//     for (int i = 0; i < MyArray.Length; i++)
+//     {
+//        if(MyArray[i] > max) max = MyArray[i];
+
+//        if(MyArray[i] < min) min = MyArray[i];
+//     }
+// }
+
+// ArrayCheck(MyArray);
+// Console.WriteLine(" -> " + (max - min));
 
