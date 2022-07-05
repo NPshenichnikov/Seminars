d520ee9eff4b49aa053a8cf998d7d4e98c808bb9
«Nikolay <pshenichnikovnf@mail.ru>
2022-07-05 22:18:59 +0300

    Работа на семинаре

---
+++ GB_Sem_8/Program.cs
@@ -0,0 +1,161 @@
+﻿//  Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+// int[,] CreateRandomArray(int m, int n, int min, int max)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void SwapArray(int[,] array)
+// {
+//     int buf = 0;
+//     int lastRow = array.GetLength(0) - 1;
+//     for (int column = 0; column < array.GetLength(1); column++)
+//     {
+//         buf = array[0, column];
+//         array[0, column] = array[lastRow, column];
+//         array[lastRow, column] = buf;
+//     }
+// }
+
+// Console.WriteLine("Enter number of rows");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of columns");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter numbe
+
+
+//  Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] SwapArray(int[,] array)
+{
+    int[,] ArrayNew = new int[array.GetLength(0), array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            ArrayNew[j, i] = array[i,j];
+        }
+    }
+    Console.WriteLine();
+    return ArrayNew;
+}
+
+
+
+ShowArray(SwapArray(myArray));
+
+//  Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+int[,] CreateRandomArray(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void ShowArrayTwo(int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ShowArrayOne(int[] array)
+{
+    // Console.WriteLine();
+    // for (int i = 0; i < array.Length; i++)
+    // {
+    //     Console.Write(array[i] + " ");
+    // }
+    // Console.WriteLine();
+    Console.WriteLine(String.Join(" ",array));
+}
+
+int [] GetOneArray(int[,] arrayTwo)
+{
+    int [] arrayOne = new int[arrayTwo.GetLength(0)*arrayTwo.GetLength(1)];
+    int index = 0;
+
+    for (int i = 0; i < arrayTwo.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayTwo.GetLength(1); j++)
+        {
+            arrayOne[index] = arrayTwo[i,j];
+            index++;
+        }
+    }
+    return arrayOne;
+}
+
+void SortArray(int[] array)
+{
+    int buf = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[i] > array[j])
+            {
+                buf = array[i];
+                array[i] = array[j];
+                array[j] = buf;
+            }
+        }
+    }
+}
+
+void PrintCounElement(int[] array)
+{
+    int count = 1;
+    int temp = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if(array[i]!=temp)
+        {
+            Console.WriteLine($"{temp} встречается {count} раз");
+            temp = array[i];
+            count = 1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"{temp} встречается {count} раз");
+}
--- Homework_7/Program.cs
+++ Homework_7/Program.cs
@@ -43,6 +43,65 @@
 
 //  Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
+int[,] CreateRandomArray(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return array;
+} 
+
+int[,] myArray = CreateRandomArray(5, 7, 1, 10);
+
+void ShowArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+    Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+ShowArray(myArray);
+
+Console.Write("Введите число строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+void Numarray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i + 1 == rows && j + 1 == columns)
+            Console.Write(rows + "," + columns +" ==> " + array[i, j] + " ");
+            if(rows > i + 1 || columns > j +) 
+            Console.Write(rows + "," + columns +" ==> " + " такого числа в массиве нет");
+        }
+
+        
+               
+    }
+    
+   }
+     
+Numarray(myArray);
+
+
+//  Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
 // int[,] CreateRandomArray(int m, int n, int min, int max)
 // {
 //     int[,] array = new int[m, n];
@@ -52,7 +111,6 @@
 //         {
 //             array[i, j] = new Random().Next(min, max + 1);
 //         }
-
 //     }
 //     return array;
 // }
@@ -66,41 +124,43 @@
 //         {
 //             Console.Write(array[i, j] + " ");
 //         }
-//     Console.WriteLine();
+//         Console.WriteLine();
 //     }
-//     Console.WriteLine();
 // }
 
-// Console.Write("Введите число строк: ");
-// int rows = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число столбцов: ");
-// int columns = Convert.ToInt32(Console.ReadLine());
+// void ArithMean(int[,] array)
 
-// void Numarray(int[,] array){
-//     for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     int sum = 0;
+    
+    
+//     for (int j = 0; j < array.GetLength(1); j++)
 //     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             if(i == rows && j == columns)
-//             Console.Write(i + "," + j +" ==> " + array[i, j] + " ");
+//       for (int i = 0; i < array.GetLength(0); i++)
+//       {
+        
+//          sum += array[0, j];   
             
-//         }
+//       }
+        
+     
+//       Console.Write(sum + " ");  
+      
 //     }
-//     if(rows > 5 || columns > 5) 
-//     Console.Write(rows + "," + columns +" ==> " + " такого числа в массиве нет");
-   
+    
 // }
 
-// int[,] myArray = CreateRandomArray(5, 5, 1, 10);
+// Console.Write("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter min number: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter max number: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandomArray( rows, columns, min, max);
 
 // ShowArray(myArray);
-// Numarray(myArray);
 
-
-//  Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-
-
-
-
-
+// ArithMean(myArray);
