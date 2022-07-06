34be42306218bf56eab23aa7d579cee9b25cc555
«Nikolay <pshenichnikovnf@mail.ru>
2022-07-07 00:16:21 +0300

    Домашняя работа 7

--- GB_Sem_8/Program.cs
+++ GB_Sem_8/Program.cs
@@ -46,116 +46,165 @@
 
 //  Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
 
-int[,] SwapArray(int[,] array)
-{
-    int[,] ArrayNew = new int[array.GetLength(0), array.GetLength(1)];
+// int[,] CreateRandomArray(int m, int n, int min, int max)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return array;
+// }
 
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            ArrayNew[j, i] = array[i,j];
-        }
-    }
-    Console.WriteLine();
-    return ArrayNew;
-}
+// void ShowArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] SwapArray(int[,] array)
+// {
+//     int[,] ArrayNew = new int[array.GetLength(0), array.GetLength(1)];
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             ArrayNew[j, i] = array[i,j];
+//         }
+//     }
+//     Console.WriteLine();
+//     return ArrayNew;
+// }
 
 
 
-ShowArray(SwapArray(myArray));
+// ShowArray(SwapArray(myArray));
 
 //  Задача 57: Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
 
-int[,] CreateRandomArray(int m, int n, int min, int max)
-{
-    int[,] array = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            array[i, j] = new Random().Next(min, max + 1);
-        }
-    }
-    return array;
-}
+// int[,] CreateRandomArray(int m, int n, int min, int max)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return array;
+// }
 
-void ShowArrayTwo(int[,] array)
-{
-    Console.WriteLine();
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
+// void ShowArrayTwo(int[,] array)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
 
-void ShowArrayOne(int[] array)
-{
-    // Console.WriteLine();
-    // for (int i = 0; i < array.Length; i++)
-    // {
-    //     Console.Write(array[i] + " ");
-    // }
-    // Console.WriteLine();
-    Console.WriteLine(String.Join(" ",array));
-}
+// void ShowArrayOne(int[] array)
+// {
+//     // Console.WriteLine();
+//     // for (int i = 0; i < array.Length; i++)
+//     // {
+//     //     Console.Write(array[i] + " ");
+//     // }
+//     // Console.WriteLine();
+//     Console.WriteLine(String.Join(" ",array));
+// }
 
-int [] GetOneArray(int[,] arrayTwo)
-{
-    int [] arrayOne = new int[arrayTwo.GetLength(0)*arrayTwo.GetLength(1)];
-    int index = 0;
+// int [] GetOneArray(int[,] arrayTwo)
+// {
+//     int [] arrayOne = new int[arrayTwo.GetLength(0)*arrayTwo.GetLength(1)];
+//     int index = 0;
 
-    for (int i = 0; i < arrayTwo.GetLength(0); i++)
-    {
-        for (int j = 0; j < arrayTwo.GetLength(1); j++)
-        {
-            arrayOne[index] = arrayTwo[i,j];
-            index++;
-        }
-    }
-    return arrayOne;
-}
+//     for (int i = 0; i < arrayTwo.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arrayTwo.GetLength(1); j++)
+//         {
+//             arrayOne[index] = arrayTwo[i,j];
+//             index++;
+//         }
+//     }
+//     return arrayOne;
+// }
 
-void SortArray(int[] array)
-{
-    int buf = 0;
+// void SortArray(int[] array)
+// {
+//     int buf = 0;
 
-    for (int i = 0; i < array.Length; i++)
-    {
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if(array[i] > array[j])
-            {
-                buf = array[i];
-                array[i] = array[j];
-                array[j] = buf;
-            }
-        }
-    }
-}
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[i] > array[j])
+//             {
+//                 buf = array[i];
+//                 array[i] = array[j];
+//                 array[j] = buf;
+//             }
+//         }
+//     }
+// }
 
-void PrintCounElement(int[] array)
-{
-    int count = 1;
-    int temp = array[0];
+// void PrintCounElement(int[] array)
+// {
+//     int count = 1;
+//     int temp = array[0];
 
-    for (int i = 1; i < array.Length; i++)
-    {
-        if(array[i]!=temp)
-        {
-            Console.WriteLine($"{temp} встречается {count} раз");
-            temp = array[i];
-            count = 1;
-        }
-        else
-        {
-            count++;
-        }
-    }
-    Console.WriteLine($"{temp} встречается {count} раз");
-}
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         if(array[i]!=temp)
+//         {
+//             Console.WriteLine($"{temp} встречается {count} раз");
+//             temp = array[i];
+//             count = 1;
+//         }
+//         else
+//         {
+//             count++;
+//         }
+//     }
+//     Console.WriteLine($"{temp} встречается {count} раз");
+
+// }
+
+
+// Console.WriteLine("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter min number: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max number: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+
+// int[,] myArray = CreateRandomArray(rows, columns, min, max);
+
+// ShowArrayTwo(myArray);
+// int[] arrayOne = GetOneArray(myArray);
+// Console.WriteLine(String.Join(" ",arrayOne));
+// SortArray(arrayOne);
+// Console.WriteLine(String.Join(" ",arrayOne));
+// PrintCounElement(arrayOne);
+
+
+
--- Homework_7/Program.cs
+++ Homework_7/Program.cs
@@ -1,30 +1,30 @@
 ﻿//  Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-// int[,] CreateRandomArray(int m, int n)
+// double[,] RandomArray(int m, int n, int min, int max)
 // {
-//     int[,] array = new int[m, n];
+//     double[,] result = new double[m, n];
 //     for (int i = 0; i < m; i++)
 //     {
 //         for (int j = 0; j < n; j++)
 //         {
-//             array[i, j] = new Random().NextDouble();
+//             result[i, j] = new Random().NextDouble()*10;
 //         }
 
 //     }
-//     return array;
+//     return result;
 
 // }
 
 
 
-// void ShowArray(int[,] array)
+// void PrintArray(double[,] array)
 // {
 
 //     for (int i = 0; i < array.GetLength(0); i++)
 //     {
 //         for (int j = 0; j < array.GetLength(1); j++)
 //         {
-//             Console.Write(array[i, j] + " ");
+//             Console.Write($"{array[i, j]:f1} ");
 //         }
 //     Console.WriteLine();
 //     }
@@ -35,72 +35,18 @@
 // int rows = Convert.ToInt32(Console.ReadLine());
 // Console.Write("Введите число столбцов: ");
 // int columns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите минимальное значение: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное значение: ");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-// int[,] myArray = CreateRandomArray(rows, columns);
 
-// ShowArray(myArray);
+// double[,] array = RandomArray(rows, columns, min, max);
+
+// PrintArray(array);
 
 
 //  Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-int[,] CreateRandomArray(int m, int n, int min, int max)
-{
-    int[,] array = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            array[i, j] = new Random().Next(min, max + 1);
-        }
-
-    }
-    return array;
-} 
-
-int[,] myArray = CreateRandomArray(5, 7, 1, 10);
-
-void ShowArray(int[,] array)
-{
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-    Console.WriteLine();
-    }
-    Console.WriteLine();
-}
-
-ShowArray(myArray);
-
-Console.Write("Введите число строк: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число столбцов: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-
-void Numarray(int[,] array){
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            if(i + 1 == rows && j + 1 == columns)
-            Console.Write(rows + "," + columns +" ==> " + array[i, j] + " ");
-            if(rows > i + 1 || columns > j +) 
-            Console.Write(rows + "," + columns +" ==> " + " такого числа в массиве нет");
-        }
-
-        
-               
-    }
-    
-   }
-     
-Numarray(myArray);
-
-
-//  Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 // int[,] CreateRandomArray(int m, int n, int min, int max)
 // {
@@ -111,9 +57,12 @@ Numarray(myArray);
 //         {
 //             array[i, j] = new Random().Next(min, max + 1);
 //         }
+
 //     }
 //     return array;
 // }
+
+// int[,] myArray = CreateRandomArray(5, 7, 1, 10);
 
 // void ShowArray(int[,] array)
 // {
@@ -126,29 +75,37 @@ Numarray(myArray);
 //         }
 //         Console.WriteLine();
 //     }
+//     Console.WriteLine();
 // }
 
-// void ArithMean(int[,] array)
+// ShowArray(myArray);
 
+// Console.Write("Введите число строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число столбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// void Numarray(int[,] array)
 // {
-//     int sum = 0;
-    
-    
-//     for (int j = 0; j < array.GetLength(1); j++)
+
+//  for (int i = 0; i < array.GetLength(0); i++)
 //     {
-//       for (int i = 0; i < array.GetLength(0); i++)
-//       {
-        
-//          sum += array[0, j];   
-            
-//       }
-        
-     
-//       Console.Write(sum + " ");  
-      
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (rows == i + 1 && columns == j + 1)
+//                 Console.Write(rows + "," + columns + " ==> " + array[i, j] + " ");
+//         }
+
 //     }
-    
+//     if (rows > array.GetLength(0) || columns > array.GetLength(1))
+//         Console.Write(rows + "," + columns + " ==> " + "такого числа в массиве нет");
+
 // }
+
+// Numarray(myArray);
+
+
+//  Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 // Console.Write("Enter number of rows: ");
 // int rows = Convert.ToInt32(Console.ReadLine());
@@ -159,8 +116,63 @@ Numarray(myArray);
 // Console.Write("Enter max number: ");
 // int max = Convert.ToInt32(Console.ReadLine());
 
-// int[,] myArray = CreateRandomArray( rows, columns, min, max);
+// int[,] array = GetArray( rows, columns, min, max);
 
-// ShowArray(myArray);
+// PrintArray(array);
 
-// ArithMean(myArray);
+// double[] averageColumns = GetResultArray(array);
+
+// Console.WriteLine($"Среднее арифметическое каждого столбца = {String.Join("; ",averageColumns)}");
+
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] array)
+// {
+//    for (int i = 0; i < array.GetLength(0); i++)
+//    {
+//        for (int j = 0; j < array.GetLength(1); j++)
+//        {
+//             Console.Write(array[i, j] + "    ");
+//        }
+//         Console.WriteLine();
+//    }
+// }
+
+
+
+// double[] GetResultArray(int[,] inArray)
+
+// {
+//     double[] result = new double[array.GetLength(1)];
+
+
+//     for (int column = 0; column < array.GetLength(1); column++)
+//     {
+
+//      double sum = 0;
+
+//         for (int rows = 0; rows < array.GetLength(0); rows++)
+//         {
+
+//          sum += array[rows, column];   
+        
+//         }
+//         result[column] = Math.Round(sum/array.GetLength(0),2);
+//     }
+
+//     return result; 
+
+// }
+    
+
